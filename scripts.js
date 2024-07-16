aec30fd1ebd92768c05ed55f746378db62f198e1
Breburda Dejan <dejan@breburda.at>
2024-07-16 14:08:32 +0200

    adding feedback

--- scripts.js
+++ scripts.js
@@ -14,5 +14,3 @@ document.getElementById('age').textContent = age;
 
 const aboutMeContent = `Hallo! Ich bin Dejan Breburda, ${age} Jahre alt und lebe in Österreich. Derzeit besuche ich noch die Schule und programmiere leidenschaftlich gerne in meiner Freizeit. Mein Fokus liegt auf Python, aber ich arbeite auch mit anderen Programmiersprachen wie JavaScript, C++, C, C# und Java. Programmieren ist für mich nicht nur ein Hobby, sondern auch eine Möglichkeit, kreative Lösungen für komplexe Probleme zu finden. Ich freue mich darauf, neue Technologien zu erkunden und innovative Projekte zu realisieren.`;
 document.getElementById('about-me-content').textContent = aboutMeContent;
-
-        
---
+++ voting.js
@@ -0,0 +1,17 @@
+
+
+function submit(){
+	document.getElementById("error").innerHTML = "";
+	var design = document.querySelector('input[name="design"]:checked');
+	var creativity = document.querySelector('input[name="creativity"]:checked');
+	var userfriendliness = document.querySelector('input[name="userfriendliness"]:checked');
+	var quality = document.querySelector('input[name="quality"]:checked');
+	var interactivity = document.querySelector('input[name="interactivity"]:checked');
+	
+	if (!(design === null || creativity === null || userfriendliness === null || quality === null || interactivity === null)){
+		window.location.replace("thanks.html")
+	}
+	else{
+		document.getElementById("error").innerHTML = 'Sie müssen alle felder ausfüllen, welche nicht als optional gekennzeichnet sind!';
+	}
+}
